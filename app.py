bf403cebd5e2b0158f25c08e60d6cf44d0766476
rxthew <richardtheumaster@gmail.com>
2023-04-17 20:56:28 +0200

    Introduce basic homepage based on search

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flask import render_template
+
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return '<html><body><p>hello world</p></body></html>'
+def index():
+    return render_template('index.html')
